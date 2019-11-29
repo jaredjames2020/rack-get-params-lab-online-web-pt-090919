958662b8d4846b9e5857563f9215829473ab60f1
Jared S <jaredjames2020@gmail.com>
2019-11-29 21:26:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,12 @@ class Application
         end
       end
       elsif req.path.match(/add/)
-      search_term = req.params["item"]
+        search_term = req.params["item"]
+        if @@items.include?(search_term)
+          resp.write "#{search_term} is one of our items"
+        else
+          resp.write "Couldn't find #{search_term}"
+        end
       
     else
       resp.write "Path Not Found"

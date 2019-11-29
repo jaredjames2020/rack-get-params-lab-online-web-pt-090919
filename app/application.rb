22d7b09ab60a8706fad9ba103afa4cb21f82bd2b
Jared S <jaredjames2020@gmail.com>
2019-11-29 21:25:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
       elsif req.path.match(/add/)
-      item = req.params["q"]
+      search_term = req.params["item"]
       
     else
       resp.write "Path Not Found"
